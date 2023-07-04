25f365059b1471f3f9f52a57a69df5498f256a7c
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2023-07-04 14:35:50 +0200

    testing retrivial chats

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,11 +106,11 @@ from langchain.text_splitter import CharacterTextSplitter
 loader = Docx2txtLoader("data/test.docx")
 # st.write(loader)
 data = loader.load()
-st.write(data[0].page_content)
+# st.write(data[0].page_content)
 
 text_splitter = CharacterTextSplitter(chunk_size=700, chunk_overlap=100)
 documents = text_splitter.split_documents(data)
-st.write(documents)
+st.write(documents[0])
 
 from langchain.embeddings import OpenAIEmbeddings
 
@@ -145,16 +145,45 @@ vectordb.persist()
 from langchain.chains import RetrievalQA
 # from langchain.llms import OpenAI
 
-qa_chain = RetrievalQA.from_chain_type(
-    llm=OpenAI(),
-    retriever=vectordb.as_retriever(search_kwargs={'k': 7}),
+# qa_chain = RetrievalQA.from_chain_type(
+#     llm=OpenAI(),
+#     retriever=vectordb.as_retriever(search_kwargs={'k': 7}),
+#     return_source_documents=True
+# )
+
+# we can now execute queries against our Q&A chain
+# result = qa_chain({'query': 'Who is the CV about?'})
+# print(result['result'])
+# st.write(result['result'])
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+
+qa_chain = ConversationalRetrievalChain.from_llm(
+    ChatOpenAI(),
+    vectordb.as_retriever(search_kwargs={'k': 6}),
     return_source_documents=True
 )
 
-# we can now execute queries against our Q&A chain
-result = qa_chain({'query': 'Who is the CV about?'})
-print(result['result'])
-st.write(result['result'])
+import sys
+
+chat_history = []
+while True:
+    # this prints to the terminal, and waits to accept an input from the user
+    query = input('Prompt: ')
+    # give us a way to exit the script
+    if query == "exit" or query == "quit" or query == "q":
+        print('Exiting')
+        sys.exit()
+    # we pass in the query to the LLM, and print out the response. As well as
+    # our query, the context of semantically relevant information from our
+    # vector store will be passed in, as well as list of our chat history
+    result = qa_chain({'question': query, 'chat_history': chat_history})
+    print('Answer: ' + result['answer'])
+    # we build up the chat_history list, based on our question and response
+    # from the LLM, and the script then returns to the start of the loop
+    # and is again ready to accept user input.
+    chat_history.append((query, result['answer']))
 
 # Import Python REPL tool and instantiate Python agent
 
