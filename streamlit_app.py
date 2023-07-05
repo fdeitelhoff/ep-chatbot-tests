33716e039bc58dd71cff0ecbc436774bf765d605
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2023-07-05 14:07:57 +0200

    bug fixing

--- streamlit_app.py
+++ streamlit_app.py
@@ -186,7 +186,7 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 
 qa_chain = ConversationalRetrievalChain.from_llm(
-    ChatOpenAI(),
+    ChatOpenAI(modelName = "gpt-3.5-turbo"),
     vectordb.as_retriever(search_kwargs={'k': 6}),
     return_source_documents=True
 )
